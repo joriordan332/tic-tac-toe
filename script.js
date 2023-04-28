abef710290e8b7e006efba5113a55feb43373caf
Jonathan O'Riordan <jooriordan@hotmail.com>
2023-04-28 12:09:53 +0100

    Added combinations array and winner functions
    WinningCombinations array added that contains all the possible sequences in which a player can win
    Winner functions for player 'X' and player 'O' added that check if the innerHTML of each square contains that players marker in any of the sequence of combinations
    Div added that states which textContent appears in once one of the players has won
    Added conditions to the if statement so that markers cant be added on to the board once someone has been declared the winner or an alert won't pop up stating that square is already taken

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
+const winningConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 const Gameboard = (() => {
-  let gameboard = ['','','','','','','','',''];
+  let gameboard = ['A1','A2','A3','B1','B2','B3','C1','C2','C3'];
   for (let i = 0; i < gameboard.length; i++) {
     const board = document.querySelector('.board');
     const boardSquares = document.createElement('div');
     boardSquares.classList.add('boardSquares');
-    boardSquares.innerHTML = gameboard[i];
     board.appendChild(boardSquares);
      
   }
@@ -21,26 +31,52 @@ const Player  = (name, marker)=>{
 };
 
 const Game = (() => {
+  let winningPlayer = document.getElementById('winningPlayer')
   const player1 = Player('Jonno', 'X');
   const player2 = Player('Abi', 'O');
   let currentPlayer = player1;
+  
   const boardSquares = document.querySelectorAll(".boardSquares");
   boardSquares.forEach((square) => {
     square.addEventListener('click', () => {
-      if (square.innerHTML != "") {
+      let move = square.id;
+      if (square.innerHTML != ""&& winningPlayer.textContent == "") {
         alert('Please click on an empty square')
       }
-      if (square.innerHTML == "" && currentPlayer == player1) {
+      if (square.innerHTML == "" && currentPlayer == player1 && winningPlayer.textContent == "") {
         square.innerHTML = player1.marker;
         currentPlayer = player2;
-        
-      } else if (square.innerHTML == "" && currentPlayer == player2) {
+       
+      }  if (square.innerHTML == "" && currentPlayer == player2 && winningPlayer.textContent == "") {
         square.innerHTML = player2.marker;
         currentPlayer = player1;
-      } 
-      
+      }  
+      if (checkWinOCell()) {
+         winningPlayer.textContent  = 'Jonno wins'
+      }
+      if (checkWinXCell()) {
+        winningPlayer.textContent  = 'Abi wins'
+      }
     })
   })
+  
+  function checkWinXCell() {
+    return winningConditions.some((combination) => {
+      return combination.every((i) => {
+        return boardSquares[i].innerText === "X";
+      });
+    });
+  }
+
+  function checkWinOCell() {
+    return winningConditions.some((combination) => {
+      return combination.every((i) => {
+        return boardSquares[i].innerText === "O";
+        
+      });
+    });
+  }
+
   
 })();
 
